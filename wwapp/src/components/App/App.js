28fd1d4daf9a6c2ede6cb4e13c9ba9810d947af1
William Browning <wbrowning@hotmail.co.uk>
2023-06-06 16:44:11 +0100

    HomePage staging edit

--- wwapp/src/components/App/App.js
+++ wwapp/src/components/App/App.js
@@ -1,12 +1,15 @@
 
 import './App.css';
+import HomePage from '../HomePage/HomePage.js'; 
 
 function App() {
   return (
     <div className="App">
       <h1>Wise Wallet Kitchen</h1>
+      <HomePage />
     </div>
   );
 }
+
 
 export default App;
