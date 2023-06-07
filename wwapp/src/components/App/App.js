4014704c4e1b42f45af3b19c7e4602720aa649f5
Jason <jason5donoghue@gmail.com>
2023-06-07 07:48:27 +0100

    first commit on main so make restrictions

--- wwapp/src/components/App/App.js
+++ wwapp/src/components/App/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>Wise Wallet Kitchen</h1>
+      <img src='https://media.tenor.com/LjhaLjAdrQgAAAAM/winnie-the-pooh-hungry.gif' alt='hungry pooh' />
     </div>
   );
 }
