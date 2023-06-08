c29f7d8bbb5bd1341968bb9f869a8770262270da
William Browning <wbrowning@hotmail.co.uk>
2023-06-08 13:57:30 +0100

    Double button edit on graph.js

--- wwapp/src/components/Wallet/graph.js
+++ wwapp/src/components/Wallet/graph.js
@@ -1,22 +1,36 @@
 import React from "react";
 import CanvasJSReact from "@canvasjs/react-charts";
-import dummyData from "./data.json";
 import "./Wallet.css";
 
 const CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
-const userSavings = 20.05
-
 class Graph extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showLineGraph: false,
+      showBarGraph: false,
+      userSavings: 20.05,
+    };
+  }
+
+  handleWeeklySavingsClick = () => {
+    this.setState({ showLineGraph: false, showBarGraph: true });
+  };
+
+  handleMonthlySavingsClick = () => {
+    this.setState({ showBarGraph: false, showLineGraph: true });
+  };
+
   render() {
-    const options = {
+    const { showLineGraph, showBarGraph, userSavings } = this.state;
+
+    const weeklySavingsOptions = {
       title: {
         text: "Weekly Savings Chart",
       },
-
       axisY: {
         minimum: 0,
-        //   maximum: 90
       },
       data: [
         {
@@ -30,17 +44,48 @@ class Graph extends React.Component {
       ],
     };
 
+    const monthlySavingsOptions = {
+      title: {
+        text: "Monthly Savings Chart",
+      },
+      axisY: {
+        minimum: 0,
+      },
+      data: [
+        {
+          type: "line",
+          dataPoints: [
+            // Hardcoded data for 30 days of savings
+            { label: "Week 1", y: 50 },
+            { label: "Week 2", y: 70 },
+            { label: "Week 3", y: 100 },
+            // Add more data points for the remaining days
+            // ...
+            { label: "Day 30", y: 200 },
+          ],
+        },
+      ],
+    };
+
     return (
       <div>
-        <CanvasJSChart
-          options={options}
-          containerProps={{ width: "100%", height: "100%" }}
-        />
+        <button onClick={this.handleWeeklySavingsClick}>Weekly Savings</button>
+        <button onClick={this.handleMonthlySavingsClick}>Monthly Savings</button>
+        {showBarGraph && (
+          <CanvasJSChart
+            options={weeklySavingsOptions}
+            containerProps={{ width: "100%", height: "100%" }}
+          />
+        )}
+        {showLineGraph && (
+          <CanvasJSChart
+            options={monthlySavingsOptions}
+            containerProps={{ width: "100%", height: "100%" }}
+          />
+        )}
       </div>
     );
   }
 }
 
 export default Graph;
-
-//
