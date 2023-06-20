003ed477b02fb44a89e664e779b4852d64649f6b
tom-soc14 <tcgs@protonmail.com>
2023-06-20 13:48:53 +0100

    Initial tests and MemoryRouter

--- wwapp/src/components/App/App.test.js
+++ wwapp/src/components/App/App.test.js
@@ -1,8 +1,33 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import {test, expect} from "@jest/globals";
+import { MemoryRouter } from 'react-router-dom';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/Wallet/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+
+
+test('renders <h1> title on home page', () => {
+  render(
+    <MemoryRouter initalEntries={['/']}>
+        <App />
+   </MemoryRouter>
+  );
+  const titleElement = screen.getByText("Welcome to Wise Wallet Kitchen");
+  expect(titleElement).toBeInTheDocument();
 });
+
+test('Sign up button on Home Page', () => {
+  render(
+    <MemoryRouter initalEntries={['/']}>
+        <App />
+   </MemoryRouter>
+  );
+  const signUpButton = screen.getByTestId('signUpBtn');
+  expect(signUpButton).toBeInTheDocument();
+});
+
