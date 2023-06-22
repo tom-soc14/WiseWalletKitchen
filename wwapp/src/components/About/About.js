c453f7beed6888f860dcff82521c8b548e48fafc
Jason <jason5donoghue@gmail.com>
2023-06-22 16:33:04 +0100

    added carasl to about page

--- wwapp/src/components/About/About.js
+++ wwapp/src/components/About/About.js
@@ -2,12 +2,12 @@
 // import walletExample from "./wallet.jpeg";
 
 import { Link } from "react-router-dom";
-import AboutImageCarousel from "./AboutChildren/AboutImageCarousel.js";
+import AboutStoryImages from "./AboutChildren/AboutStoryImages.js";
 
 function About() {
   return (
     <div className="about-container">
-      <AboutImageCarousel />
+      <AboutStoryImages />
       <Link className="Link-button-sign-up" to="/login">
         <button className="HomePage-button-sign-up">Sign Up Today</button>
       </Link>
