1a7f298c75f1ff45fa63a3796c2397a4954d099c
nick v <nvalente.electrics@gmail.com>
2023-06-23 16:56:04 +0100

    continued working on the re-direct to HomePage with Logout =>  still not working

--- wwapp/src/components/Login/Logout.js
+++ wwapp/src/components/Login/Logout.js
@@ -2,49 +2,45 @@
 import "./Login.css";
 // import { ThemeSupa } from "@supabase/auth-ui-shared";
 import { supabase } from "./Login.js";
-// import { useNavigate } from "react-router";
+import { useNavigate } from "react-router";
 
 
 // TO DO - Research the following function, the move into burger menu ??
 export default function Logout() {
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
 
   const handleLogout = async () => {
     try {
       await supabase.auth.signOut();
+      
       // Perform any additional logic or state updates after successful logout
       console.log("Log out was successful...!");
       // Navigate back to the HomePage
-      // navigate("/");
+      navigate("/");
     } catch (error) {
       console.error("Error logging out:", error);
     }
   };
 
-  // useEffect(() => {
-    
-  //     //NEW - Call the handleLogIn props to pass up for logInAccess ✅
-  //     handleLogout();
-  //     // Redirect to homepage after a short delay
-      
-  //       navigate("/");
-     
-  //     return () => {
-        
-  //       console.log("logout session reset data. Test 3 ✅")
-  //     };
-  //   })
-
+  
   //NEW - removed button and called function everytime
   handleLogout();
-
+  
   return null;
 }
 
-// const redirect = () => {
-//   navigate("/");
-// };
-// return () => {
-//   console.log("login session reset data. Test 2 ✅");
-//   redirect();
-// };
+
+
+// useEffect(() => {
+  
+//     //NEW - Call the handleLogIn props to pass up for logInAccess ✅
+//     handleLogout();
+//     // Redirect to homepage after a short delay
+    
+//       navigate("/");
+   
+//     return () => {
+      
+//       console.log("logout session reset data. Test 3 ✅")
+//     };
+//   })
