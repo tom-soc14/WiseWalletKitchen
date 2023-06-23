2ed6b012e070577c267fd370c512c3abc055c594
nick v <nvalente.electrics@gmail.com>
2023-06-23 14:42:44 +0100

    tried to re-direct back to homepage after logout

--- wwapp/src/components/Login/Logout.js
+++ wwapp/src/components/Login/Logout.js
@@ -1,31 +1,35 @@
 import React from "react";
 import "./Login.css";
-import { useState, useEffect } from "react";
-import { createClient } from "@supabase/supabase-js";
-import { Auth } from "@supabase/auth-ui-react";
 // import { ThemeSupa } from "@supabase/auth-ui-shared";
-import {supabase} from "./Login.js"
-
-// const supabase = createClient(
-//     "https://vdwwjhldkqhbmwtszcas.supabase.co",
-//     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZkd3dqaGxka3FoYm13dHN6Y2FzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODYxNTA4MjYsImV4cCI6MjAwMTcyNjgyNn0.5ATbh7xJDjVK57XPGsJmkwxps0fV0iyoOCRvUU2VMlc"
-//   );
+import { supabase } from "./Login.js";
+import { useNavigate } from "react-router";
 
 // TO DO - Research the following function, the move into burger menu ??
 export default function Logout() {
-    const handleLogout = async () => {
-      try {
-        await supabase.auth.signOut();
-        // Perform any additional logic or state updates after successful logout
-        console.log("Log out was successful...!")
-      } catch (error) {
-        console.error("Error logging out:", error);
-      }
-    };
+  const navigate = useNavigate();
 
-    //NEW - removed button and called function everytime
-    handleLogout()
+  const handleLogout = async () => {
+    try {
+      await supabase.auth.signOut();
+      // Perform any additional logic or state updates after successful logout
+      console.log("Log out was successful...!");
+      // Navigate back to the HomePage
+      navigate("/");
+    } catch (error) {
+      console.error("Error logging out:", error);
+    }
+  };
 
+  //NEW - removed button and called function everytime
+  handleLogout();
 
-    return ;
-  }
+  return null;
+}
+
+// const redirect = () => {
+//   navigate("/");
+// };
+// return () => {
+//   console.log("login session reset data. Test 2 ✅");
+//   redirect();
+// };
