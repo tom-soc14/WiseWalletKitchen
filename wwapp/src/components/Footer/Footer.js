69c00d8ffd175018c35061f5292acd7417220a15
Katie <s.k.smith@live.co.uk>
2023-06-26 14:36:40 +0100

    footer updates to CSS

--- wwapp/src/components/Footer/Footer.js
+++ wwapp/src/components/Footer/Footer.js
@@ -1,27 +1,81 @@
-import owlLogo from "./owl-tiny.png"
+import owlLogo from "./owl-tiny.png";
 
 function Footer() {
-    return(
-        <div className="footer-container">
-            <footer>
-            
-            <div className = "inside-footer-container">
-            
-            {/* <p>Smart Eating and Savings</p> */}
+  return (
+    <div className="footer-container">
+      <footer>
+        <div className="footer-content">
+          <div className="owl-image">
             <img className="owl-logo" src={owlLogo} alt="logo" />
-            
-            <div className="github-links-container">Created by:
-            <a className = "github-profile" href="https://github.com/Ksmi23" target="_blank" rel="noreferrer">Katie</a>
-            <a className = "github-profile" href="https://github.com/wbrowning01" target="_blank" rel="noreferrer">Will</a>
-            <a className = "github-profile" href="https://github.com/Jason5D" target="_blank" rel="noreferrer">Jason</a>
-            <a className = "github-profile" href="https://github.com/tom-soc14" target="_blank" rel="noreferrer">Tom</a>
-            <a className = "github-profile" href="https://github.com/Ice-and-Rock" target="_blank" rel="noreferrer">Nick</a>
-            <a className = "github-profile" href="https://github.com/bianca-g-b" target="_blank" rel="noreferrer">Bianca</a>
-            </div>
-            </div>
-      </footer>
+          </div>
+          <div className="footer-links">
+            <h1>Contact Us</h1>
+            <h1>Our Team</h1>
+          </div>
+ <div className="social-media-buttons">
+    <img src="" alt="pinterest button"/>
+
+    <img src="" alt="twitter button" />
+    <img src="" alt="youtube button"/>
+ </div>
+          
         </div>
-    )
+      </footer>
+    </div>
+  );
 }
 
 export default Footer;
+
+
+/* <div className="github-links-container">
+            Created by:
+            <a
+              className="github-profile"
+              href="https://github.com/Ksmi23"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Katie
+            </a>
+            <a
+              className="github-profile"
+              href="https://github.com/wbrowning01"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Will
+            </a>
+            <a
+              className="github-profile"
+              href="https://github.com/Jason5D"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Jason
+            </a>
+            <a
+              className="github-profile"
+              href="https://github.com/tom-soc14"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Tom
+            </a>
+            <a
+              className="github-profile"
+              href="https://github.com/Ice-and-Rock"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Nick
+            </a>
+            <a
+              className="github-profile"
+              href="https://github.com/bianca-g-b"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Bianca
+            </a>
+          </div> */
