4a8f3e2cc7017073529ca6c7d8662db300f45192
nick v <nvalente.electrics@gmail.com>
2023-06-27 19:51:14 +0100

    added photos to OurTeam and completed cards css

--- wwapp/src/components/OurTeamPage/OurTeamPage.js
+++ wwapp/src/components/OurTeamPage/OurTeamPage.js
@@ -1,5 +1,11 @@
 import "./OurTeamPage.css"
 import MaryBerryHOTTT from './MaryBerryHOTTT.jpg'
+import HeadshotNick from './TeamPhotos/Headshot-Nick2.png'
+import HeadshotKatie from './TeamPhotos/Headshot-Katie.png'
+import HeadshotBianca from './TeamPhotos/Headshot-Bianca.png'
+import HeadshotJason from './TeamPhotos/Headshot-Jason.png'
+import HeadshotWill from './TeamPhotos/Headshot-Will.png'
+import SpongeBob from './TeamPhotos/SpongeBob.jpg'
 
 function OurTeamPage () {
     return (
@@ -20,9 +26,50 @@ function OurTeamPage () {
     <div class="row">
   <div class="column">
     <div class="card">
-      <img src={MaryBerryHOTTT} alt="Jane" />
+      <img src={HeadshotNick} alt="Nick" />
       <div class="container">
-        <h2>Jane Doe</h2>
+        <h2>Nick Valente</h2>
+        <p class="title">Big Dogg &amp; Tech Support</p>
+        <p>Electrician with a passion for mountain sports.</p>
+        <p>nvalente.electrics@gmail.com</p>
+        <p><button class="button">Contact</button></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <img src={HeadshotBianca} alt="Bianca" />
+      <div class="container">
+        <h2>Bianca Buha</h2>
+        <p class="title">The true mastermind</p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+        <p>example@example.com</p>
+        <p><button class="button">Contact</button></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <img src={SpongeBob} alt="Tom" />
+      <div class="container">
+        <h2>Tom Swapp</h2>
+        <p class="title">Delete Master & Top Critic</p>
+        <p>The knower of all API knowledge and wild theories</p>
+        <p>tomswapp@example.com</p>
+        <p><button class="button">Contact</button></p>
+      </div>
+    </div>
+  </div>
+
+
+
+  <div class="column">
+    <div class="card">
+      <img src={HeadshotKatie} alt="Katie" />
+      <div class="container">
+        <h2>Katie Smith</h2>
         <p class="title">CEO &amp; Founder</p>
         <p>Some text that describes me lorem ipsum ipsum lorem.</p>
         <p>example@example.com</p>
@@ -33,10 +80,10 @@ function OurTeamPage () {
 
   <div class="column">
     <div class="card">
-      <img src={MaryBerryHOTTT} alt="Mike" />
+      <img src={HeadshotJason} alt="Jason" />
       <div class="container">
-        <h2>Mike Ross</h2>
-        <p class="title">Art Director</p>
+        <h2>Jason Donehue</h2>
+        <p class="title">Chief Whip & big brain</p>
         <p>Some text that describes me lorem ipsum ipsum lorem.</p>
         <p>example@example.com</p>
         <p><button class="button">Contact</button></p>
@@ -46,51 +93,10 @@ function OurTeamPage () {
 
   <div class="column">
     <div class="card">
-      <img src={MaryBerryHOTTT} alt="John" />
+      <img src={HeadshotWill} alt="Will" />
       <div class="container">
-        <h2>John Doe</h2>
-        <p class="title">Designer</p>
-        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-        <p>example@example.com</p>
-        <p><button class="button">Contact</button></p>
-      </div>
-    </div>
-  </div>
-
-
-
-  <div class="column">
-    <div class="card">
-      <img src={MaryBerryHOTTT} alt="Jane" />
-      <div class="container">
-        <h2>Jane Doe</h2>
-        <p class="title">CEO &amp; Founder</p>
-        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-        <p>example@example.com</p>
-        <p><button class="button">Contact</button></p>
-      </div>
-    </div>
-  </div>
-
-  <div class="column">
-    <div class="card">
-      <img src={MaryBerryHOTTT} alt="Mike" />
-      <div class="container">
-        <h2>Mike Ross</h2>
-        <p class="title">Art Director</p>
-        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-        <p>example@example.com</p>
-        <p><button class="button">Contact</button></p>
-      </div>
-    </div>
-  </div>
-
-  <div class="column">
-    <div class="card">
-      <img src={MaryBerryHOTTT} alt="John" />
-      <div class="container">
-        <h2>John Doe</h2>
-        <p class="title">Designer</p>
+        <h2>Will Browning</h2>
+        <p class="title">Leader & freedom fighter</p>
         <p>Some text that describes me lorem ipsum ipsum lorem.</p>
         <p>example@example.com</p>
         <p><button class="button">Contact</button></p>
